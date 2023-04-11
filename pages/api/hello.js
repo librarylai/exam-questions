5dab0b77f24e7fbad5407cccbad6ee4d9a0d6fa8
librarylai <library0515@gmail.com>
2023-04-11 17:32:32 +0800

    add ckEditor local upload image

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs'
+import formidable from 'formidable'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
+
+const handler = async (req, res) => {
+  const form = new formidable.IncomingForm()
+  form.parse(req, async (err, fields, file) => {
+    if (err) {
+      return res.status(500).json({ error: '解析 FormData 数据失败' })
+    }
+    const mimetype = file?.files?.mimetype
+    // 未來用 GCP 或 AWS 來存檔
+    fs.readFile(file.files.filepath, (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: '讀取文件失敗' })
+      }
+      // 将文件内容转换为 base64
+      const base64Data = data.toString('base64')
+      // 在这里可以对 base64Data 进行处理，例如保存到数据库或进行其他业务逻辑处理
+      return res.status(200).json({ file: `data:${mimetype};base64,${base64Data}` })
+    })
+  })
+}
+export default handler
--- src/components/CKEditor/Editor.js
+++ src/components/CKEditor/Editor.js
@@ -1,10 +1,48 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import React from 'react'
-
+const API_URL = `/api/hello`
 const Editor = ({ value, onChange, ...props }) => {
+  function uploadAdapter(loader) {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData()
+          loader.file.then((file) => {
+            body.append('files', file)
+            // let headers = new Headers()
+            // headers.append('content-type', 'multipart/form-data')
+            fetch(API_URL, {
+              method: 'post',
+              body: body,
+              //   headers: headers,
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                console.log('res', res)
+                resolve({
+                  //   default: `${API_URL}/${res?.filename}`,
+                  default: `data:base64:${res?.file}`,
+                })
+              })
+              .catch((err) => {
+                reject(err)
+              })
+          })
+        })
+      },
+    }
+  }
+  function uploadPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return uploadAdapter(loader)
+    }
+  }
   return (
     <CKEditor
+      config={{
+        extraPlugins: [uploadPlugin],
+      }}
       editor={ClassicEditor}
       data={value}
       onChange={(event, editor) => {
